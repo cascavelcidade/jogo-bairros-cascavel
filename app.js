68dd479f7aad948377e790b3d3a1b2ca3ca642e1
cascavelcidade <machaves@minha.fag.edu.br>
2025-12-16 20:53:56 -0300

    Update app.js

--- app.js
+++ app.js
@@ -251,3 +251,11 @@ init().catch(err => {
   console.error(err);
   toast("Erro ao carregar os dados. Verifique se bairros.geojson está na pasta.");
 });
+const toggleBtn = document.getElementById("toggleSidebar");
+const sidebar = document.querySelector(".sidebar");
+
+if (toggleBtn && sidebar) {
+  toggleBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("hidden");
+  });
+}
